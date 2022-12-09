75a974e266b0cff8aaed3a0d4442e1c1595330c6
Oskar <uczen@ckziu.jaworzno.pl>
2022-12-09 13:44:27 +0100

    final result

--- szyfr.py
+++ szyfr.py
@@ -1,9 +1,20 @@
 tekst = input("Podaj słowo do zaszyfrowania:")
-szyfr = ""
+zaszyfrowane = ""
 for i in range(len(tekst)):
     klucz = i+1
     if ord(tekst[i]) > 122 - klucz:
-        szyfr += chr(ord(tekst[i]) + klucz - 26)
+        zaszyfrowane += chr(ord(tekst[i]) + klucz - 26)
     else:
-        szyfr += chr(ord(tekst[i]) + klucz)
-print("Zaszyfrowany tekst to: ", szyfr)
+        zaszyfrowane += chr(ord(tekst[i]) + klucz)
+print("Zaszyfrowany tekst to: ", zaszyfrowane)
+
+odszyfrowane = ""
+for i in range(len(zaszyfrowane)):
+    klucz = i+1
+    if ord(zaszyfrowane[i]) > 122 + klucz:
+        odszyfrowane += chr(ord(zaszyfrowane[i]) - klucz - 26)
+    else:
+        odszyfrowane += chr(ord(zaszyfrowane[i]) - klucz)
+print("Odszyfrowany tekst to: ", odszyfrowane)
+
+
