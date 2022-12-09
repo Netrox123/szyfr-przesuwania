bc5bd14de2430ff0e8e11b907b6d92a442484345
Oskar <uczen@ckziu.jaworzno.pl>
2022-12-09 14:00:01 +0100

    final

--- szyfr.py
+++ szyfr.py
@@ -1,20 +1,26 @@
 tekst = input("Podaj słowo do zaszyfrowania:")
-zaszyfrowane = ""
-for i in range(len(tekst)):
-    klucz = i+1
-    if ord(tekst[i]) > 122 - klucz:
-        zaszyfrowane += chr(ord(tekst[i]) + klucz - 26)
-    else:
-        zaszyfrowane += chr(ord(tekst[i]) + klucz)
-print("Zaszyfrowany tekst to: ", zaszyfrowane)
 
-odszyfrowane = ""
-for i in range(len(zaszyfrowane)):
-    klucz = i+1
-    if ord(zaszyfrowane[i]) > 122 + klucz:
-        odszyfrowane += chr(ord(zaszyfrowane[i]) - klucz - 26)
-    else:
-        odszyfrowane += chr(ord(zaszyfrowane[i]) - klucz)
-print("Odszyfrowany tekst to: ", odszyfrowane)
+def szyfrowanie():
+    zaszyfrowane = ""
+    for i in range(len(tekst)):
+        klucz = i+1
+        if ord(tekst[i]) > 122 - klucz:
+            zaszyfrowane += chr(ord(tekst[i]) + klucz - 26)
+        else:
+            zaszyfrowane += chr(ord(tekst[i]) + klucz)
+    return zaszyfrowane
 
+def rozszyfrowanie():
+    odszyfrowane = ""
+    for i in range(len(szyfrowanie())):
+        klucz = i+1
+        if ord(szyfrowanie()[i]) > 122 + klucz:
+            odszyfrowane += chr(ord(szyfrowanie()[i]) - klucz - 26)
+        else:
+            odszyfrowane += chr(ord(szyfrowanie()[i]) - klucz)
+    print("Odszyfrowany tekst to: ", odszyfrowane)
+
+print("Rozszyfrowany tekst to: ", szyfrowanie())
+
+rozszyfrowanie()
 
